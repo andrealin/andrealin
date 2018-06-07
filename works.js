21536884e017392765fb76d8a48aca45e2bb1cb7
Andrea Lin <andrealin2018@gmail.com>
2018-06-06 23:06:15 -0600

    Initial commit

---
+++ works.js
@@ -0,0 +1,22 @@
+function insertParagraph( paragraph, id ) {
+	var element = document.getElementById(id);
+	paragraph.addElement( paragraph );
+}
+
+function addLineBreaks( lines ) {
+  var paragraph;
+  return paragraph;
+}
+
+function addIndents( paragraph ) {
+  return paragraph;
+}
+
+window.onload = function() {
+  var client =
+  var memoirPoemLines;
+  insertParagraph( addLineBreaks( memoirPoemLines ), 'memoirpoem' );
+
+  var masqueradeShortStoryLines;
+  insertParagraph( addIndents( addLineBreaks( masqueradeShortStoryLines ) ), 'masqueradeshortstory' );
+}
